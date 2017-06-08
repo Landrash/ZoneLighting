854647572fcd10a969eb025f6dc6044b7bd6629b
Anshul Vishwakarma <shootdaj@gmail.com>
2017-06-08 09:03:46 -0700

    Added Codemap

--- ZoneLighting/Usables/RunnerHelpers.cs
+++ ZoneLighting/Usables/RunnerHelpers.cs
@@ -240,7 +240,7 @@ namespace ZoneLighting.Usables
 	    public static Zone CreateNodeMCUZone(ZLM zlm)
 	    {
             var zone  = ZoneScaffolder.Instance.AddNodeMCUZone(zlm.Zones, "NodeMCUZone",
-                64, 1);
+                6, 1);
 
             return zone;
         }
